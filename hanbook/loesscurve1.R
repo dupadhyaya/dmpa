7e89a7c168235d544cf6c91acc8a1a48d8da1896
dupadhyaya <dup1966@gmail.com>
2017-02-27 08:25:24 +0530

    1

--- hanbook/loesscurve1.R
+++ hanbook/loesscurve1.R
@@ -1,12 +1,12 @@
 # Loess Curve
 # Scatter Plot - better perception of pattern of dependence
 
-rm(list=ls()
+rm(list=ls())
 
 require(graphics)
 data(cars)
 plot(cars)
-?scatter.smooth
+#?scatter.smooth
 with(cars, scatter.smooth(speed, dist))
 ## or with dotted thick smoothed line results :
 with(cars, scatter.smooth(speed, dist, lpars =
--- hanbook/raster1.R
+++ hanbook/raster1.R
@@ -17,3 +17,48 @@ s <- stack(x=c(r, r*2, r))
 b <- brick(x=c(r, r*2, r))
 # Let's look at the properties of of one of these two objects
 b
+
+# Start by making sure that your working directory is properly set
+# If not you can set it using setwd()
+getwd()
+download.file(url = 'https://raw.githubusercontent.com/GeoScripting-WUR/IntroToRaster/gh-pages/data/gewata.zip', destfile = 'gewata.zip', method = 'auto')
+# In case the download code doesn't work, use method = 'wget'
+## Unpack the archive
+unzip('gewata.zip')
+
+# When passed without arguments, list.files() returns a character vector, listing the content of the working directory
+list.files()
+# To get only the files with .tif extension
+list.files(pattern = glob2rx('*.tif'))
+# Or if you are familiar with regular expressions
+list.files(pattern = '^.*\\.tif$')
+
+gewata <- brick('LE71700552001036SGS00_SR_Gewata_INT1U.tif')
+gewata 
+
+plot(gewata, 1)
+e <- drawExtent(show=TRUE)
+
+## Crop gewata using e
+gewataSub <- crop(gewata, e)
+## Now visualize the new cropped object
+plot(gewataSub, 1)
+
+# Again, make sure that your working directory is properly set
+getwd()
+## Download the data
+download.file(url='https://raw.githubusercontent.com/GeoScripting-WUR/IntroToRaster/gh-pages/data/tura.zip', destfile='tura.zip', method='auto')
+unzip(zipfile='tura.zip')
+## Retrieve the content of the tura sub-directory
+list <- list.files(path='tura/', full.names=TRUE)
+
+plot(raster(list[1]))
+
+turaStack <- stack(list)
+turaStack
+
+writeRaster(x=turaStack, filename='turaStack.grd', datatype='INT2S')
+
+writeRaster(x=turaStack, filename='turaStack.grd', datatype='INT2S')
+
+plot(ndvi)
